70f492107dd4087567b970e91da2ced7a86511c6
MichelleMayambu <your_email@example.com>
2020-01-22 11:26:37 +0200

    CreatedCustomerDto

---
+++ WebApplication2/Dtos/CustomerDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using WebApplication2.Models;
+
+namespace WebApplication2.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "please enter customer name")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Min18YearsMember]
+        public DateTime? BirthDate { get; set; }
+
+        public string Gender { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+      //removed reference objects and models to create DTo's for them
+
+    }
+}
